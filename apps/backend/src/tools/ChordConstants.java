1092218d4bd3abf54e99b375004de2838d588493
MiguelGarcia5605 <100482998+MiguelGarcia5605@users.noreply.github.com>
2025-12-12 07:03:36 -0600

    changed folder structure

---
+++ apps/backend/src/tools/ChordConstants.java
@@ -0,0 +1,10 @@
+package tools;
+
+
+public class ChordConstants {
+    public static final int
+        MAJOR_SEVENTH = 0,
+        MINOR_SEVENTH = 1,
+        DOMINANT_SEVENTH = 2,
+        HALF_DIMINISHED_SEVENTH = 3;
+}
